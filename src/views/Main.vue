20d454d07453297ba99459d46760e8e8cc8e004c
wentingliu <cindy810317@gmail.com>
2022-04-02 17:51:20 +0800

    feat: add addColor() & submitAnswer() function

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -2,28 +2,41 @@
   <div class="main">
     <div class="main_title">flag quiz game</div>
     <div class="main_country">
-      <div class="name">belgium</div>
-      <div class="continent">(europe)</div>
+      <div class="name">{{ dummyData[currentLevel].country }}</div>
+      <div class="continent">({{dummyData[currentLevel].continent}})</div>
     </div>
 
-    <div class="main_block">
-      <div class="block one">one</div>
-      <div class="block two">two</div>
-      <div class="block three">three</div>
+    <div
+      :class="showComponent"
+      class="main_wrapper"
+    >
+      <div class="main_block">
+        <div
+          v-for="(block, index) in blockColors"
+          :key="index"
+          :class="{active: currentBlock === index + 1}"
+          class="block"
+          :style="{'background-color': block}"
+        ></div>
+      </div>
+
+      <div class="main_color">
+        <div 
+          v-for="color in colors"
+          :key="color"
+          class="color"
+          :class="color"
+          @click="addColor(color)"
+        >
+        </div>
+      </div>
     </div>
 
-    <div class="main_color">
-      <div class="color red" style="background-color: var(--red);"></div>
-      <div class="color orange" style="background-color: var(--orange);"></div>
-      <div class="color yellow" style="background-color: var(--yellow);"></div>
-      <div class="color green" style="background-color: var(--green);"></div>
-      <div class="color blue-light" style="background-color: var(--blue-light);"></div>
-      <div class="color blue" style="background-color: var(--blue);"></div>
-      <div class="color white" style="background-color: var(--white);"></div>
-      <div class="color black" style="background-color: var(--black);"></div>
-    </div>
-
-    <button class="main_button btn">
+    <button 
+      class="main_button btn"
+      :disabled="btnDisabled"
+      @click="submitAnswer"
+    >
       submit
     </button>
 
@@ -33,6 +46,55 @@
 
 <script>
 export default {
-  name: 'Main'
+  name: 'Main',
+  data() {
+    return {
+      showComponent: 'horizontal',
+      currentLevel: 0,
+      currentBlock: 1,
+      colors: ['red', 'orange', 'yellow', 'green', 'blue-light', 'blue', 'white', 'black'],
+      blockColors: ['', '', ''],
+      btnDisabled: true,
+      dummyData: [
+        {
+          level: 1,
+          country: 'belgium',
+          continent: 'europe',
+          blockColors: ['black', 'yellow', 'red']
+        },
+        {
+          level: 2,
+          country: 'chad',
+          continent: 'africa',
+          blockColors: ['blue', 'yellow', 'red']
+        },
+        {
+          level: 3,
+          country: 'france',
+          continent: 'europe',
+          blockColors: ['blue', 'white', 'red']
+        }
+      ]
+    }
+  },
+  methods: {
+    addColor(color) {
+      if (this.currentBlock === 0) return
+      this.blockColors[this.currentBlock - 1] = color
+      this.currentBlock ++
+
+      if (this.currentBlock === 4) {
+        this.currentBlock = 0
+        this.btnDisabled = false
+      }
+    },
+    submitAnswer() {
+      if (JSON.stringify(this.blockColors) === JSON.stringify(this.dummyData[this.currentLevel].blockColors)) {
+        console.log('Correct!')
+      } else {
+        console.log('false')
+      }
+    }
+  }
 }
 </script>
