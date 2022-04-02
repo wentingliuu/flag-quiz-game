6e4cc6ff8f63122e3c95e4b9bc297196872f8e88
wentingliu <cindy810317@gmail.com>
2022-04-02 14:26:34 +0800

    feat: add Main.vue HTML & CSS

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,5 +1,34 @@
 <template>
-  <h1>Main</h1>
+  <div class="main">
+    <div class="main_title">flag quiz game</div>
+    <div class="main_country">
+      <div class="name">belgium</div>
+      <div class="continent">(europe)</div>
+    </div>
+
+    <div class="main_block">
+      <div class="block one">one</div>
+      <div class="block two">two</div>
+      <div class="block three">three</div>
+    </div>
+
+    <div class="main_color">
+      <div class="color red" style="background-color: var(--red);"></div>
+      <div class="color orange" style="background-color: var(--orange);"></div>
+      <div class="color yellow" style="background-color: var(--yellow);"></div>
+      <div class="color green" style="background-color: var(--green);"></div>
+      <div class="color blue-light" style="background-color: var(--blue-light);"></div>
+      <div class="color blue" style="background-color: var(--blue);"></div>
+      <div class="color white" style="background-color: var(--white);"></div>
+      <div class="color black" style="background-color: var(--black);"></div>
+    </div>
+
+    <button class="main_button btn">
+      submit
+    </button>
+
+    <p class="copy-right">©2022 WENTING LIU</p>
+  </div>
 </template>
 
 <script>
